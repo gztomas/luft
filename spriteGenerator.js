a45b1b4745f332dfd71882a1a23ab4f13402ca80
gztomas <gztomas@gmail.com>
2013-03-31 22:46:50 -0300

    Sprite generator now usable

--- spriteGenerator.js
+++ spriteGenerator.js
@@ -8,39 +8,89 @@ new function() {
 		document.body.insertBefore(button, document.body.firstChild);
 	};
 
-	var loadImage = function(src) {
-		var img = document.createElement("img");
+	var loadImage = function(imageData, callback) {
+		var img = new Image();
 		img.onload = function() {
-			canvas.width = img.width;
-			canvas.height = img.height;
-			context.drawImage(img, 0, 0);
+			img.offset = width;
+			img.name = imageData[1];
+			img.alpha = imageData[2];
+			width += img.width;
+			if(img.height > height)
+				height = img.height;
+			images.push(img);
+			callback();
 		};
-		img.src = src;
-		document.body.appendChild(img);
+		img.src = imageData[0];
 	};
 
-	var width = 200;
-	var height = 200;
+	var loadImages = function(images) {
+		var index = -1;
+		var loadCurrent = function() {
+			index++;
+			if(index < images.length)
+				loadImage(images[index], loadCurrent);
+		};
+		loadCurrent();
+	};
 
-	var canvas = document.createElement("canvas");
-	canvas.style.marginLeft = 'auto';
-	canvas.style.marginRight = 'auto';
-	canvas.style.marginTop = 'auto';
-	canvas.style.marginBottom = 'auto';
-	canvas.width = width;
-	canvas.height = height;
+	var width = 0;
+	var height = 0;
+	var images = [];
+	var canvas;
 
-	var context = canvas.getContext("2d");
-	var image = context.createImageData(width, height);
+	var result = document.getElementById("result");
+	result.value += "{\n";
 
-	document.body.insertBefore(canvas, document.body.firstChild);
-
-	loadImage("image/nave1big.bmp");
+	loadImages([
+		["image/nave1big.bmp", "blackShipDemo", true],
+		["image/nave2big.bmp", "silverShipDemo", true],
+		["image/versus.bmp", "versus", true],
+		["image/nave1.bmp", "blackShip", true],
+		["image/nave2.bmp", "silverShip", true],
+		["image/explo.bmp", "explosion", true],
+		["image/laser1.bmp", "rocket", true],
+		["image/laser2.bmp", "laser", true],
+		["image/jugar.bmp", "play", true],
+		["image/salir.bmp", "exit", true],
+		["image/backmenu.bmp", "mountains", false],
+		["image/fondo.bmp", "space", false],
+		["image/intro.bmp", "fight", false],
+		["image/empate.bmp", "draw", false],
+		["image/ganador.bmp", "winner", false]
+	]);
 
 	createButton("export", function() {
 		var resultBase64 = canvas.toDataURL("image/a.tiff");
 		var img = document.createElement("img");
 		img.src = resultBase64;
 		document.body.appendChild(img);
+	});
+	createButton("draw", function() {
+		canvas = document.createElement("canvas");
+		canvas.style.marginLeft = 'auto';
+		canvas.style.marginRight = 'auto';
+		canvas.style.marginTop = 'auto';
+		canvas.style.marginBottom = 'auto';
+		canvas.width = width;
+		canvas.height = height;
+		var context = canvas.getContext("2d");
+		document.body.insertBefore(canvas, document.body.firstChild);
+
+		for(var j = 0; j < images.length; j++) {
+			var img = images[j];
+			context.drawImage(img, img.offset, 0);
+			var imageData = context.getImageData(img.offset, 0, img.width, img.height);
+			var pixelData = imageData.data;
+			for(var i = 0; i < pixelData.length && img.alpha; i+=4) {
+				if(!pixelData[i] && !pixelData[i+1] && !pixelData[i+2])
+					pixelData[i+3] = 0;
+			}
+			context.putImageData(imageData, img.offset, 0);
+			result.value += "\t" + img.name + ": {x: " + img.offset + ", y: 0, width: " + img.width + ", height: " + img.height + "}";
+			if(j == images.length - 1)
+				result.value += "\n}";
+			else
+				result.value += ",\n";
+		}
 	});
 };
