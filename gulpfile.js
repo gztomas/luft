3c453b80496337431ec15429e9ac7adcdb24380d
gztomas <gztomas@gmail.com>
2016-06-23 20:47:51 -0300

    Add uglify and livereload to build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var livereload = require('gulp-livereload');
+
+var scriptsSource = './src/*.js';
 
 gulp.task('scripts', function() {
-  return gulp.src('./src/*.js')
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('./dist/'));
+  return gulp.src(scriptsSource)
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'))
+    .pipe(livereload());
 });
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(scriptsSource, ['scripts']);
+});
+
+gulp.task('default', ['scripts']);
