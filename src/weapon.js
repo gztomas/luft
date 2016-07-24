42b7a6b69ad0fa0d681928dfe9ed032f89379ab4
gztomas <gztomas@gmail.com>
2016-07-24 20:03:32 -0300

    Migrate weapon class to ES6

--- src/weapon.js
+++ src/weapon.js
@@ -3,30 +3,36 @@
 import { Resources } from './resources';
 import { Renderable, SpaceObject } from './renderable';
 
-export function Weapon(world, type) {
-  var _this = this;
-  var _renderable;
+export class Weapon {
 
-  this.draw = function() {
-    return _renderable.draw.apply(_renderable, arguments);
-  };
-  this.deploy = function(x, y, angle) {
-    var image = type == 1 ? Resources.sprite.rocket : Resources.sprite.laser;
-    _renderable = new Renderable(image, 0, {
+  constructor(world, type) {
+    this.world = world;
+    this.type = type;
+  }
+
+  draw() {
+    return this.renderable.draw.apply(this.renderable, arguments);
+  }
+
+  deploy(x, y, angle) {
+    var image = this.type == 1 ? Resources.sprite.rocket : Resources.sprite.laser;
+    this.renderable = new Renderable(image, 0, {
       width: 40, height: 40, x: x, y: y, angle: angle, z: 0, speed: 20, angularSpeed: 0
     });
-    SpaceObject.apply(this, [_renderable.state]);
-  };
-  this.notifyAfterCalculation = function() {
-    if (_renderable.state.y < -_renderable.state.height ||
-      _renderable.state.y > world.height + _renderable.state.height ||
-      _renderable.state.x < -_renderable.state.width ||
-      _renderable.state.x > world.width + _renderable.state.width) {
-      world.remove(_this);
+    SpaceObject.apply(this, [this.renderable.state]);
+  }
+
+  notifyAfterCalculation() {
+    if (this.renderable.state.y < -this.renderable.state.height ||
+      this.renderable.state.y > this.world.height + this.renderable.state.height ||
+      this.renderable.state.x < -this.renderable.state.width ||
+      this.renderable.state.x > this.world.width + this.renderable.state.width) {
+      this.world.remove(this);
     }
-  };
-  this.notifyCollision = function(/*target*/) {
+  }
+
+  notifyCollision(/*target*/) {
     //if(target.shipID != _this.ownerID)
-    //  world.remove(_this);
+    //  this.world.remove(_this);
   };
 }
